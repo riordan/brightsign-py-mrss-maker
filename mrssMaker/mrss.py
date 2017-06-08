ea98a67863270706a578be220f7c5eb0550d86a8
David Riordan <dr@daveriordan.com>
2017-06-08 11:00:58 -0400

    wip

--- mrssMaker/mrss.py
+++ mrssMaker/mrss.py
@@ -7,6 +7,9 @@ from six.moves.urllib.parse import urljoin
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 import sys
+from PIL import Image
+import imageio
+#from error import IOError
 
 """
 python mrss.py http://baseurl/maybe/the/media/folder/ http://baseurl/maybe/the/feed.xml targetfolder/
@@ -30,29 +33,44 @@ def prettify(elem):
 
 def generateMRSS():
     parser = argparse.ArgumentParser(description='Create a Brightsign MRSS feed from a directory of images')
-    parser.add_argument("base_url", help="Default location of MRSS assets when hosted (eg http://...01/content/media/)")
-    parser.add_argument("feed_url", help="Default location of MRSS feed assets when hosted (eg http://...01/content/feed.xml)")
+    parser.add_argument("base_url",
+                        help="Default location of MRSS assets when hosted (eg http://...01/content/media/)")
+    parser.add_argument("feed_url",
+                        help="Default location of MRSS feed assets when hosted (eg http://...01/content/feed.xml)")
     parser.add_argument("media_folder", help="folder of media assets")
+    parser.add_argument("--thumbnail_folder", action="store", default=False,
+                        help="media_folder/thumbnails folder. Should be under your media_folder. Will be created if doesn't already exist")
 
 
     if len(sys.argv[1:])==0:
-        print("Need help?")
         parser.print_help()
         # parser.print_usage() # for just the usage line
         parser.exit()
 
     args = parser.parse_args()
-
+    thumbs_dir = args.thumbnail_folder
     base_directory = args.media_folder
+
+    if not os.path.exists(base_directory):
+        raise IOError('Your media folder does not exist')
+
+    if thumbs_dir and os.path.abspath(base_directory) not in os.path.abspath(thumbs_dir):
+        raise IOError('Your thumbnail directory is not in the right path')
 
     filelist = []
     for root,directories, filenames, in os.walk(base_directory):
         for filename in filenames:
             if ".DS_Store" in filename:
                 pass
+            #Skip anything from within thumbnails folder
+        else if thumbs_dir (thumbs_dir in root):
+                print(os.path.join(root, filename))
+                pass
             else:
                 filepath = os.path.join(root, filename)
                 filelist.append(MRImage(filepath, args.base_url))
+
+    #print(filelist)
 
 
     ###############
@@ -136,5 +154,5 @@ def generateMRSS():
         else:
             pass
 
-    print(prettify(rss))
+    #print(prettify(rss))
 generateMRSS()
